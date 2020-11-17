4ef56918f126deb2a1c283eda1af620de7d16bc8
Nabeil Lemar <nabeillemar@gmail.com>
2020-11-17 03:08:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
+  
+attr_accessor :new, :total
+  
+def initialize 
+  @total = 0
+end 
 
+def add_item(title, price)
+ return @total + price
+end 
+
+def apply_discount 
+  
+end 
+
+def items
+end 
+
+def void_last_transaction
+end 
+
+end 
